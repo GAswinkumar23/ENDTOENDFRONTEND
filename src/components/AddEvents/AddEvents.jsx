f9a5eafafa5c4f1828ca7c15b7cb033a6e60403c
GAswinkumar23 <g.aswin1001@gmail.com>
2025-08-17 19:02:13 +0530

    validate date and time

--- src/components/AddEvents/AddEvents.jsx
+++ src/components/AddEvents/AddEvents.jsx
@@ -2,27 +2,38 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import '../../styles/DashboardComponent/AddEvent.css';
-const AddEvents = ({userid}) => {
-    const [eventName, setEventName]= useState("");
+const AddEvents = ({ userid }) => {
+    const [eventName, setEventName] = useState("");
     const [eventDescription, setEventDescription] = useState("");
     const [eventdate, setEventDate] = useState("");
     const [eventTime, setEventTime] = useState("");
     const [eventPriority, setEventPriority] = useState("low");
     const navigate = useNavigate();
-    const handlehubmit=(e)=> 
-    {
+    const handlehubmit = (e) => {
         e.preventDefault();
+        const eventDateTimeString = `${eventdate}T${eventTime}`;
+
+        const eventDateTime = new Date(eventDateTimeString);
+
+        const now = new Date();
+
+     
+        if (eventDateTime <= now) {
+            console.error("Validation Failed: The event time must be in the future.");
+            alert("Validation Failed: The event time must be in the future.");
+            return false;
+        }
         axios.post('http://localhost:5000/api/eventadd', {
             title: eventName,
             description: eventDescription,
             date: eventdate,
             time: eventTime,
             priority: eventPriority,
-             userId: userid
+            userId: userid
         }).then((response) => {
             console.log(response.data);
             alert(response.data.message);
-            if(response.data.status === 200) {
+            if (response.data.status === 200) {
                 alert("Event added successfully");
                 navigate(`/dashboard`);
             }
@@ -31,68 +42,68 @@ const AddEvents = ({userid}) => {
         });
         setEventName("");
         setEventDescription("");
-        setEventDate("");       
+        setEventDate("");
         setEventTime("");
         setEventPriority("low");
         e.target.reset();
     }
     return (<>
-            <form onSubmit={handlehubmit}>
-                <div className="form-group">
-                    <li>
-                        <ul>
-                            <label htmlFor="eventName">Event Title</label>
-                            <input type="text" 
+        <form onSubmit={handlehubmit}>
+            <div className="form-group">
+                <li>
+                    <ul>
+                        <label htmlFor="eventName">Event Title</label>
+                        <input type="text"
                             className="form-control"
-                             id="eventName" 
-                             placeholder="Enter event name"
-                             value={eventName} 
-                             onChange={(e)=>{setEventName(e.target.value)}} />
-                        </ul>
-                        <ul>
-                            <label htmlFor="eventDescription">Event Description</label>
-                            <input type="text" 
-                            className="form-control" 
-                            id="eventDescription" 
-                            placeholder="Enter event description" 
-                            value={eventDescription} 
-                            onChange={(e)=>{setEventDescription(e.target.value)}}/>
-                        </ul>
-                        <ul>
-                            <label htmlFor="eventDate">Event Date</label>
-                            <input type="date" 
-                            className="form-control" 
-                            id="eventDate" 
-                            placeholder="Enter event date" 
-                            value={eventdate} 
-                            onChange={(e)=>{setEventDate(e.target.value)}}  />
-                        </ul>
-                        <ul>
-                            <label htmlFor="eventTime">Event Time</label>
-                            <input type="time" 
-                            className="form-control" 
-                            id="eventTime" 
-                            placeholder="Enter event time" 
-                            value={eventTime} 
-                            onChange={(e)=>{setEventTime(e.target.value)}}/>
-                        </ul>
-                        <ul>
-                            <li>
-                                <label htmlFor="priority">Priority</label>
-                                <select className="form-control" id="priority" 
-                                onChange={(e)=> setEventPriority(e.target.value)} 
+                            id="eventName"
+                            placeholder="Enter event name"
+                            value={eventName}
+                            onChange={(e) => { setEventName(e.target.value) }} />
+                    </ul>
+                    <ul>
+                        <label htmlFor="eventDescription">Event Description</label>
+                        <input type="text"
+                            className="form-control"
+                            id="eventDescription"
+                            placeholder="Enter event description"
+                            value={eventDescription}
+                            onChange={(e) => { setEventDescription(e.target.value) }} />
+                    </ul>
+                    <ul>
+                        <label htmlFor="eventDate">Event Date</label>
+                        <input type="date"
+                            className="form-control"
+                            id="eventDate"
+                            placeholder="Enter event date"
+                            value={eventdate}
+                            onChange={(e) => { setEventDate(e.target.value) }} />
+                    </ul>
+                    <ul>
+                        <label htmlFor="eventTime">Event Time</label>
+                        <input type="time"
+                            className="form-control"
+                            id="eventTime"
+                            placeholder="Enter event time"
+                            value={eventTime}
+                            onChange={(e) => { setEventTime(e.target.value) }} />
+                    </ul>
+                    <ul>
+                        <li>
+                            <label htmlFor="priority">Priority</label>
+                            <select className="form-control" id="priority"
+                                onChange={(e) => setEventPriority(e.target.value)}
                                 value={eventPriority}>
-                                    <option value="low">Low</option>
-                                    <option value="medium">Medium</option>
-                                    <option value="high">High</option>
-                                </select>
-                            </li>
-                        </ul>
-                    </li>
-                    <button type="submit">Add the Event</button>
-                </div>
-                
-            </form>
+                                <option value="low">Low</option>
+                                <option value="medium">Medium</option>
+                                <option value="high">High</option>
+                            </select>
+                        </li>
+                    </ul>
+                </li>
+                <button type="submit">Add the Event</button>
+            </div>
+
+        </form>
     </>);
 }
 export default AddEvents;
